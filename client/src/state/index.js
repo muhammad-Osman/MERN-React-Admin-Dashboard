72a38df395af0f8b65bdea7924d93c69f0d7ee80
muhammad-Osman <muhammad.usman1282@outlook.com>
2023-02-11 23:05:11 +0500

    Navbar & side bar completed

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   mode: "dark",
-  userId: "63701cc1f03239b7f700000e"
+  userId: "63e357ea930bf63cfa1275ed"
 };
 
 export const globalSlice = createSlice({
