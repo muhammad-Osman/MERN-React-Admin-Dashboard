e53085e180a8f658529d6c22d9180be0199c4f67
muhammad-Osman <muhammad.usman1282@outlook.com>
2023-03-19 11:02:49 +0500

    breakdown section completed

--- client/src/scenes/breakdown/Breakdown.jsx
+++ client/src/scenes/breakdown/Breakdown.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Box } from "@mui/material";
+import Header from "components/Header";
+import BreakdownChart from "components/BreakdownChart";
 
 const Breakdown = () => {
-  return <div>Breakdown</div>;
+  return <Box m="1.5rem 2.5rem">
+    <Header title="BREAKDOWN" subTitle="Breakdown of Sales By Category." />
+    <Box mt="40px" height="75vh">
+        <BreakdownChart />
+    </Box>
+  </Box>;
 };
 
 export default Breakdown;
